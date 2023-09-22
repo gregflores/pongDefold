3a7765be4dea282860566b5d877fc0edb7de1b7e
Greg Flores <floresgreg@hotmail.com>
2023-09-22 16:06:33 -0500

    Basic Game mechanics and scoring.

--- main/ball.go
+++ main/ball.go
@@ -1,6 +1,6 @@
 components {
-  id: "paddle"
-  component: "/main/paddle.sprite"
+  id: "ball"
+  component: "/main/ball.script"
   position {
     x: 0.0
     y: 0.0
@@ -14,15 +14,35 @@ components {
   }
 }
 embedded_components {
-  id: "paddleCollision"
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/pong.atlas\"\n"
+  "default_animation: \"pong_ball\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "collisionobject"
   type: "collisionobject"
   data: "collision_shape: \"\"\n"
-  "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
-  "mass: 1.0\n"
+  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
-  "group: \"default\"\n"
-  "mask: \"default\"\n"
+  "group: \"ball\"\n"
+  "mask: \"paddle\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
@@ -41,7 +61,7 @@ embedded_components {
   "    count: 3\n"
   "  }\n"
   "  data: 12.5\n"
-  "  data: 75.0\n"
+  "  data: 12.5\n"
   "  data: 10.0\n"
   "}\n"
   "linear_damping: 0.0\n"
