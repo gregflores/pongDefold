96619aeff29cc590f4c1dd48794012ee6e8cc014
Greg Flores <floresgreg@hotmail.com>
2023-09-27 16:28:17 -0500

    Goal game object added

--- main/gameobjects/goal.go
+++ main/gameobjects/goal.go
@@ -1,49 +1,13 @@
-components {
-  id: "ball"
-  component: "/main/scripts/ball.script"
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
 embedded_components {
-  id: "sprite"
-  type: "sprite"
-  data: "tile_set: \"/main/sprites/pong.atlas\"\n"
-  "default_animation: \"pong_ball\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-embedded_components {
-  id: "collisionobject"
+  id: "wallCollision"
   type: "collisionobject"
   data: "collision_shape: \"\"\n"
   "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
   "mass: 0.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
-  "group: \"ball\"\n"
-  "mask: \"paddle\"\n"
-  "mask: \"goal\"\n"
+  "group: \"goal\"\n"
+  "mask: \"ball\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
@@ -61,8 +25,8 @@ embedded_components {
   "    index: 0\n"
   "    count: 3\n"
   "  }\n"
-  "  data: 12.5\n"
-  "  data: 12.5\n"
+  "  data: 5.0\n"
+  "  data: 1000.0\n"
   "  data: 10.0\n"
   "}\n"
   "linear_damping: 0.0\n"
